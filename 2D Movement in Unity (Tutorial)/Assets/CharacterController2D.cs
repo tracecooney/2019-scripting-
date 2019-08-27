0084f5297890e9a2bb4faf6e13da4fe3928735db
Unknown <trace.wade.cooney@gmail.com>
2019-08-27 15:13:16 -0600

    Added Unity, Modified .gitignore

---
+++ 2D Movement in Unity (Tutorial)/Assets/CharacterController2D.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class CharacterController2D
+{
+    internal void Move(float v, object fasle1, object fasle2)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ 2D Movement in Unity (Tutorial)/Assets/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Brackeys Tutorial
+
+public class PlayerController : MonoBehaviour {
+
+    public CharacterController2D controller;
+    public float runSpeed = 40f;
+    float horizontalMove = 0f;
+    private object fasle;
+
+
+    // Update is called once per frame
+    void Update ()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+	}
+
+    private void FixedUpdate()
+    {
+        // Move our character
+        controller.Move(horizontalMove * Time.fixedDeltaTime, fasle, fasle);
+    }
+}
