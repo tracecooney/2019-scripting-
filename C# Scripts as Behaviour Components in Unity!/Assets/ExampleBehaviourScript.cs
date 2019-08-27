ecd01070ceef79163c6dca79e11d27e0b444b5a7
Unknown <trace.wade.cooney@gmail.com>
2019-08-27 15:56:23 -0600

    C# Scripts as Behaviour Components in Unity!
    This is a Unity tutorial from Unity's YouTube Channel

---
+++ C# Scripts as Behaviour Components in Unity!/Assets/ExampleBehaviourScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This is a Unity tutorial from Unitys Youtube Channel
+
+public class ExampleBehaviourScript : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.R));
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+        }
+        if (Input.GetKeyDown(KeyCode.G));
+        {
+            GetComponent<Renderer>().material.color = Color.green;
+        }
+        if (Input.GetKeyDown(KeyCode.B));
+        {
+            GetComponent<Renderer>().material.color = Color.blue;
+        }
+    }
+}
