c54b0a9a04329174cc789f30b0f4c9b348acf5bc
Unknown <trace.wade.cooney@gmail.com>
2019-08-28 17:24:31 -0600

    C# Variables And Functions in Unity!

---
+++ C# Variables And Functions in Unity!/Assets/VariablesAndFunctions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariablesAndFunctions : MonoBehaviour 
+{
+	int myInt = 5;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		myInt = MultiplyByTwo(myInt);
+		Debug.Log(myInt);
+	}
+
+	int MultiplyByTwo(int number)
+	{
+		int result;
+		result = number * 2;
+		return result;
+	}
+}
