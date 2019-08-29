5bd38748099836b23f9458fade573c4f77d004cc
Unknown <trace.wade.cooney@gmail.com>
2019-08-29 15:42:09 -0600

    C# If Statements in Unity
    This is a Unity tutorial from Unity's YouTube channel.

---
+++ C# If Statements in Unity!/Assets/IfStatements.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.UIElements.GraphView;
+using UnityEngine;
+
+//this is a Unity tutorial
+public class IfStatements : MonoBehaviour
+{
+	public float coffeeTemperature = 85.0f;
+	public float hotLimitTemperature = 70.0f;
+	public float coldLimitTemperature = 40.0f;
+
+	void Update ()
+	{
+		if (Input.GetButtonDown(KeyCode.Space.ToString()))
+			TemperatureTest();
+		coffeeTemperature -= Time.deltaTime * 5f;
+	}
+
+	void TemperatureTest()
+	{
+		if (coffeeTemperature > hotLimitTemperature)
+		{
+			print("Coffee is too hot.");
+		}
+		else if (coffeeTemperature < coldLimitTemperature)
+		{
+			print("Coffee is too cold.");
+		}
+		else
+		{
+			print("Coffee is just right.");
+		}
+	}
+}
