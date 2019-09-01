2a0230908c09d91a26713f82217153c26ea6ad4b
Unknown <trace.wade.cooney@gmail.com>
2019-09-01 17:04:54 -0600

    C# Loops in Unity!
    This is a Unity Tutorial from Unity's YouTube channel

---
+++ C# Loops in Unity!/Assets/DowhileLoop.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Unity Tutorial
+public class DowhileLoop : MonoBehaviour {
+
+	
+	void Start ()
+	{
+		bool shouldContinue = false;
+
+		do
+		{
+			print("Hello World");
+		} 
+		while (shouldContinue == true);
+	}
+
+}
---
+++ C# Loops in Unity!/Assets/WhileLoop.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Unity Tutorial
+public class WhileLoop : MonoBehaviour
+{
+	private int cupsInTheSink = 4;
+	
+	void Start () 
+	{
+		while (cupsInTheSink > 0)
+		{
+			Debug.Log("I have washed a cup!");
+			cupsInTheSink--;
+		}
+	}
+}
