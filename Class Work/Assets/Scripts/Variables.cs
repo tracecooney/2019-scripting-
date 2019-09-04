21715964ee90615bd473ca1b60699d7fbf964e2d
Unknown <trace.wade.cooney@gmail.com>
2019-09-04 13:43:36 -0600

    Class Work
    Added to the Script

--- Class Work/Assets/Scripts/Variables.cs
+++ Class Work/Assets/Scripts/Variables.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using Object = UnityEngine.Object;
 
 public class Variables : MonoBehaviour
 {
@@ -13,9 +14,25 @@ public class Variables : MonoBehaviour
 	public int firePower;
 	public UnityEvent Event;
 
+	//private void OnPlayerConnected();
+	//{
+		//Event.Invoke();
+	//}
+
+	//private void OnPlayerConnected(NetworkPlayer player)
+	//{
+		//Event.Invoke();
+	//}
+
 	private void OnTriggerEnter(Collider other)
 	{
 		//throw new NotImplementedException();
 		Event.Invoke();
+		//Event.Invoke(OnPlayerConnected(Destroy(gameObject.Player)));
 	}
+
+	//private void OnParticleTrigger()
+	//{
+		//Event.Invoke();
+	//}
 }
