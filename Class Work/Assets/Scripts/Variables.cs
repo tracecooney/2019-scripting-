c09e3d1f496772f7345c0a5c7048f3f01a31fc20
Unknown <trace.wade.cooney@gmail.com>
2019-09-03 23:10:48 -0600

    Class Work 9/3/19

---
+++ Class Work/Assets/Scripts/Variables.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Variables : MonoBehaviour
+{
+
+	public float floatValue = 5f;
+	public int intValue = 20;
+	public string stringValue = "Bob11";
+	public int firePower;
+	public UnityEvent Event;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		//throw new NotImplementedException();
+		Event.Invoke();
+	}
+}
