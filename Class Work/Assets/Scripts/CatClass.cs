ff13d00bd429563d909edbceb852d0c87b011431
Unknown <trace.wade.cooney@gmail.com>
2019-09-12 15:53:15 -0600

    Class Work 9/12/19
    Added DogClass and  CatClass scripts and UnityEvents

---
+++ Class Work/Assets/Scripts/CatClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CatClass : MonoBehaviour
+{
+    public UnityEvent turnOffLight;
+    public UnityEvent explodeTheCat;
+
+    private void OnMouseDown()
+    {
+        turnOffLight.Invoke();
+        explodeTheCat.Invoke();
+    }
+}
---
+++ Class Work/Assets/Scripts/DogClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DogClass : MonoBehaviour
+{
+    public UnityEvent badDogTurnOff;
+    public UnityEvent dogUseParticle;
+
+    private void OnMouseDown()
+    {
+        badDogTurnOff.Invoke();
+        dogUseParticle.Invoke();
+    }
+    
+}
