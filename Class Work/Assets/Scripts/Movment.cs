6773863d253b6bcc5cfda7a94bcffe38d6f7bb55
Unknown <trace.wade.cooney@gmail.com>
2019-09-17 16:02:46 -0600

    Class Work 9/17/19
    Added Character Controller and Movement scripts

---
+++ Class Work/Assets/Scripts/Movment.cs
@@ -0,0 +1,39 @@
+﻿
+using UnityEngine;
+
+public class Movment : MonoBehaviour
+{
+	private Vector3 position;
+	public CharacterController controller;
+	public float speed = 1f;
+	public float gravity = 3f;
+	public float jumpSpeed = 10f;
+
+	private int jumpCount = 2;
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//transform.Translate(position*Time.deltaTime);
+		position.x = speed* Input.GetAxis("Horizontal");
+		position.y -= gravity;
+
+		if (!controller.isGrounded)
+		{
+			position.y -= gravity;
+		}
+		else
+		{
+			jumpCount = 2;
+		}
+		
+		if (Input.GetButtonDown("Jump") && jumpCount > 0)
+		{
+			position.y = jumpSpeed;
+			jumpCount--;
+		}
+		
+		controller.Move(position*Time.deltaTime);
+		//transform.Translate(position);
+	}
+}
