2c586c521617d1b5a28e0ca77a60a570831674e1
Unknown <trace.wade.cooney@gmail.com>
2019-10-01 22:15:09 -0600

    Class Work 10/1/19
    Added IntData script

---
+++ Class Work/Assets/Scripts/IntData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class IntData : ScriptableObject
+{
+    public int value = 1;
+
+    public void UpdateValue(int number)
+    {
+        value += number;
+    }
+
+}
---
+++ Class Work/Assets/Scripts/UITextController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class UITextController : MonoBehaviour
+{
+	public UnityEvent startEvent;
+	private Text textLabel;
+
+	public void UpdateText(IntData data)
+	{
+		textLabel.text = data.value.ToString();
+	}
+
+	void Start ()
+	{
+		textLabel = GetComponent<Text>();
+		startEvent.Invoke();
+	}
+	
+}
