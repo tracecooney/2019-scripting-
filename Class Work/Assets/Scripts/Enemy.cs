81ad347f386a2ec04f23395770162bb10fbde6d1
Unknown <trace.wade.cooney@gmail.com>
2019-10-13 13:39:47 -0600

    Class Work
    Working on making a Enemy

---
+++ Class Work/Assets/Scripts/Enemy.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+
+//This is a TUTORIAL i found on YouTube
+public class Enemy : MonoBehaviour
+{
+	public float speed;
+	public float stopDistance;
+	public float retreatDistance;
+	void Start ()
+	{
+		player = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	void Update () 
+	{
+		if (Vector2.Distance(transform.position, player.position) > stopDistance)
+		{
+			transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+		}
+	}
+}
