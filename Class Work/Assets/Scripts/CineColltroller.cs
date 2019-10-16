7ff77e3a12a9c7d178bd5bf97f349bf04809f925
Unknown <trace.wade.cooney@gmail.com>
2019-10-15 22:36:47 -0600

    Class Work 10/15/19
    added CineMachine script

---
+++ Class Work/Assets/Scripts/CineColltroller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(CinemachineVirtualCamera))]
+public class CineColltroller : MonoBehaviour
+{
+	public PlayerData playerData;
+	private CinemachinrVirtualCamera virtualCamera;
+
+	// Use this for initialization
+	void Start ()
+	{
+		virtualCamera = GetComponent<CinemachinrVirtualCamera>();
+		playerData.instanceAction = InstanceHandler;
+		playerData.InstancePlayer();
+	}
+
+	private void InstanceHandler(GameObject obj)
+	{
+		virtualCamera.Follow = obj.transform;
+	}
+}
--- Class Work/Assets/Scripts/DataScripts/PlayerData.cs
+++ Class Work/Assets/Scripts/DataScripts/PlayerData.cs
@@ -1,17 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 [CreateAssetMenu]
 public class PlayerData : GameArtData
 {
-    public FloatData health;
-    public List <WeaponData>  weapons;
+    public UnityAction<GameObject> instanceAction;
     
+    public FloatData health;
+    public List<WeaponData> weapons;
+
     public void InstancePlayer()
     {
         var newPlayer = Instantiate(prefab);
         var newSprite = newPlayer.GetComponentInChildren<SpriteRenderer>();
+        plyerSprite.sprite = sprite;
         newSprite.color = color;
+        instanceAction(newPlayer);
     }
 }
