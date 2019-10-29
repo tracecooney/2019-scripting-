171304be961c8a465db8e8cc19b3207511e21a09
Unknown <trace.wade.cooney@gmail.com>
2019-10-29 15:05:48 -0600

    Class Work 10/29/19
    Added EnumSwitch scripts

---
+++ Class Work/Assets/Scripts/Debugger.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class Debugger : ScriptableObject 
+{
+    public void OnDebug(string words)
+    {
+        Debug.Log((words));
+    }
+}
---
+++ Class Work/Assets/Scripts/ENUMSwitch.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ENUMSwitch : MonoBehaviour 
+{
+	public enum States
+	{
+		Start,
+		Playtime,
+		End,
+	}
+
+	public States currentStates;
+
+	public UnityEvent OnStartEvent, OnPlayerEvent, OnEndEvent;
+
+	private void Update()
+	{
+		switch (currentStates)
+		{
+			case States.Start:
+				OnStartEvent.Invoke();
+				break;
+			case States.Playtime:
+				OnPlayerEvent.Invoke();
+				break;
+			case States.End:
+				OnEndEvent.Invoke();
+				break;
+			default:
+				throw new ArgumentOutOfRangeException();
+		}
+	}
+}
