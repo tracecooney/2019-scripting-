d2dd59d24f786f77c499825f1bbb079346ff29ee
Unknown <trace.wade.cooney@gmail.com>
2019-10-29 12:29:14 -0600

    Class Work
    Added JumpThroughPlatform prefab and JumpThrough script

---
+++ Class Work/Assets/Scripts/JumpThrough.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpThrough : MonoBehaviour
+{
+	public Collider platform;
+	private void OnTriggerEnter(Collider other)
+	{
+		platform.enabled = false;
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		platform.enabled = true;
+	}
+}
