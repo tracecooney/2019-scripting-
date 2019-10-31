1244d04338f116d312164b45135c92b353da487d
Unknown <trace.wade.cooney@gmail.com>
2019-10-31 16:00:45 -0600

    Class Work AI 10/31/19
    Halloween!

---
+++ Class Work AI/Assets/Scripts/AI/AgentHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent((typeof(NavMeshAgent)))]
+public class AgentHandler : MonoBehaviour
+{
+	private NavMeshAgent agent;
+	private Transform currentDestioation;
+	public Transform destinationObj;
+	private GameObject startObj;
+	
+	void Start ()
+	{
+		startObj = new GameObject();
+		startObj.transform.position = transform.position;
+		currentDestioation = transform;
+		agent = GetComponent<NavMeshAgent>();
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		currentDestioation = destinationObj;
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		currentDestioation = startObj.transform;
+	}
+
+	void Update ()
+	{
+		agent.destination = currentDestioation.position;
+	}
+}
