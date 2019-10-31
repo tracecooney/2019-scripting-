312e326a2aef55d58f9e3591ec106509ac59261d
Unknown <trace.wade.cooney@gmail.com>
2019-10-30 19:16:52 -0600

    RPG
    Udemy Tutorial (Fixed NavMeshAgent)

---
+++ RPG/Assets/Scripts/PlayerMover.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMover : MonoBehaviour
+{
+	//This is a Udemy Tutorial
+	[SerializeField]
+	private Transform target;
+	private void Update ()
+	{
+		GetComponent<NavMeshAgent>().destination = target.position;
+	}
+}
