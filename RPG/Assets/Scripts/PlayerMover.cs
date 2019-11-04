6c30cbcc5e8a748ef5a25919d58a71b892b2ac60
Unknown <trace.wade.cooney@gmail.com>
2019-11-04 13:49:46 -0700

    RPG
    Working on Character Animation

--- RPG/Assets/Scripts/PlayerMover.cs
+++ RPG/Assets/Scripts/PlayerMover.cs
@@ -12,6 +12,7 @@ public class PlayerMover : MonoBehaviour
 		{
 			MoveToCursor();
 		}
+		UpdateAnimator();
 	}
 
 	private void MoveToCursor()
@@ -23,6 +24,14 @@ public class PlayerMover : MonoBehaviour
 		if (hasHit)
 		{
 			GetComponent<NavMeshAgent>().destination = hit.point;
+		}
+
+		private void UpdateAnimator()
+		{
+			Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+			Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+			float speed = localVelocity.z;
+			GetComponent<Animator>().SetFloat("ForwardSpeed", speed);
 		}
 	}
 }
